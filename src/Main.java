bd12e32c539222bad42b288e36b83e77ce955fd0
Demurashov <denmurolimp@yandex.ru>
2022-10-22 08:44:59 +0300

    убрал Scanner

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,9 @@
-import java.util.Scanner;
 
 public class Main {
-    static Scanner sc=new Scanner(System.in);
-    private static int[] sales={10,5,3,1,18,4,6,0};
+    private static int[] sales = {10, 5, 3, 1, 18, 4, 6, 0};
+
     public static void main(String[] args) {
         SalesManager manager = new SalesManager(sales);
-        System.out.println("Максимальный доход от продаж: "+manager.max());
+        System.out.println("Максимальный доход от продаж: " + manager.max());
     }
 }
